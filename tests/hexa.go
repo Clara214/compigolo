301cce07f6adee1967c4cdaaa2c7ef020f1be46b
Erocr <nickolafo@gmail.com>
2025-11-18 16:57:57 +0100

    test for hexadecimals

---
+++ tests/hexa.go
@@ -0,0 +1,13 @@
+package main;
+import "fmt";
+
+// Résultat attendu 42
+func main() {
+     var x, y int;
+       x = 0x120;
+       y = 0X6aF;
+       x = x+2;
+       y = y*(x+4);
+       fmt.Print(y)
+};
+
