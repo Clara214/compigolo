d459a4a362d25e568fff01cd92d845c2e27bf913
Erocr <nickolafo@gmail.com>
2025-12-02 00:20:38 +0100

    amelioation du script qui lance les tests

--- execute_tests.sh
+++ execute_tests.sh
@@ -1,7 +1,14 @@
 # if dune build; then (jai une console wsl cheloue dsl)
 
 if opam exec -- dune build;then
+    echo "Devrait réussir :"
     for f in tests/*; 
+        do echo $f; 
+        ./mgoc.exe $f; 
+    done
+
+    echo "Devrait donner une erreur :"
+    for f in tests_errors/*; 
         do echo $f; 
         ./mgoc.exe $f; 
     done
