5ace18a359e39c80fc93679a412b169297c230b9
clami94 <clara214412@gmail.com>
2025-12-07 21:57:27 +0100

    type checker + test

---
+++ tests/list_chainee.go
@@ -0,0 +1,24 @@
+package main;
+import "fmt";
+
+type Node struct {
+    val int;
+    next *Node
+};
+
+// Résultat attendu : 123
+func main() {
+    var ancre, p *Node;
+    
+    ancre = new(Node);
+    ancre.val = 1;
+    
+    ancre.next = new(Node);
+    ancre.next.val = 2;
+    
+    heancread.next.next = new(Node);
+    ancre.next.next.val = 3;
+    for (p = ancre; p != nil; p = p.next) {
+        fmt.Print(p.val)
+    }
+}
---
+++ tests/mutuel_rec.go
@@ -0,0 +1,16 @@
+package main;
+import "fmt";
+
+// Résultat attendu : 10
+func even(n int) bool {
+    if (n == 0) { return true } else { return odd(n-1) }
+};
+
+func odd(n int) bool {
+    if (n == 0) { return false } else { return even(n-1) }
+};
+
+func main() {
+    if (even(10)) { fmt.Print(1) } else { fmt.Print(0) };
+    if (odd(11)) { fmt.Print(0) } else { fmt.Print(1) }
+}
